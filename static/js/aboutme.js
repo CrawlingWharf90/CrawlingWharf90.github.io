d912f47aa0a227cadbfe8a46d04848d63d552c9f
Nadav Moscovici <71459855+CrawlingWharf90@users.noreply.github.com>
2025-03-07 09:51:43 +0100

    style: restyled about me page by removing the free time effect, centering everything, also fixed an issue that caused the education tab to misbehave

--- static/js/aboutme.js
+++ static/js/aboutme.js
@@ -14,6 +14,8 @@ const freeTimeText = "WHAT ABOUT MY FREE TIME?";
 var scrollStep = 7; 
 const saveTAAGPosition = TAAG.getBoundingClientRect();
 const saveEduPosition = edu.getBoundingClientRect();
+const baseMargin = 50;
+const heightFactor = 0.5;
 var locked = false;
 var scrollCounter = 0; 
 var lastDirection = ""; 
@@ -26,6 +28,9 @@ const elementsToCenter = [edu, TAAG, secondSection]
 document.addEventListener("DOMContentLoaded", function () {
   lastScroll = window.scrollY;
   window.scroll(0, 0);
+  //setResponsiveMargin(); 
+  let additionalMargin = window.innerHeight * heightFactor;
+  edu.style.marginTop = `${TAAG.getBoundingClientRect().height + baseMargin - additionalMargin}px`;
   freeTimeTitle.textContent = freeTimeText;
   windowHeight = window.innerHeight;
   blurAmount = 0; //! this could pontentially be a local variable since at the begginning it's always 0, but we'll keep it here just in case
@@ -57,67 +62,67 @@ function openEdu(evt, schoolName) {
   evt.currentTarget.className += " active";
 }
 
-window.addEventListener('wheel', (e) => 
-{ 
-  if (e.deltaMode === 1) {
-    //? Line mode: Likely a mouse wheel
-    scrollStep = 1; 
-  } else if (e.deltaMode === 0) {
-    //? Pixel mode: Likely a trackpad
-    scrollStep = 7; 
-  }
+// window.addEventListener('wheel', (e) => 
+// { 
+//   if (e.deltaMode === 1) {
+//     //? Line mode: Likely a mouse wheel
+//     scrollStep = 1; 
+//   } else if (e.deltaMode === 0) {
+//     //? Pixel mode: Likely a trackpad
+//     scrollStep = 7; 
+//   }
 
-  let lastScroll = virtualScrollY;
-  if(freeTimeEffect)
-  {
-    virtualScrollY += e.deltaY;
+//   let lastScroll = virtualScrollY;
+//   if(freeTimeEffect)
+//   {
+//     virtualScrollY += e.deltaY;
 
-    const currentScroll = virtualScrollY; 
+//     const currentScroll = virtualScrollY; 
 
-    if (currentScroll > lastScroll) {
-      if (lastDirection == "up") scrollCounter = 0;
-      lastDirection = "down";
-      scrollCounter++;
-      if (scrollCounter >= scrollStep) {
-        if (sectionTitle.textContent == "") sectionTitle.textContent = path;
-        const removedLetters = freeTimeText.slice(-(sectionTitle.textContent.length - path.length + 1));
-        freeTimeTitle.textContent = freeTimeTitle.textContent.slice(0, -1);
-        sectionTitle.textContent = path + removedLetters;
-        scrollCounter = 0;
-      }
-    }
+//     if (currentScroll > lastScroll) {
+//       if (lastDirection == "up") scrollCounter = 0;
+//       lastDirection = "down";
+//       scrollCounter++;
+//       if (scrollCounter >= scrollStep) {
+//         if (sectionTitle.textContent == "") sectionTitle.textContent = path;
+//         const removedLetters = freeTimeText.slice(-(sectionTitle.textContent.length - path.length + 1));
+//         freeTimeTitle.textContent = freeTimeTitle.textContent.slice(0, -1);
+//         sectionTitle.textContent = path + removedLetters;
+//         scrollCounter = 0;
+//       }
+//     }
 
-    if (currentScroll < lastScroll) {
-      if (lastDirection == "down") scrollCounter = 0;
-      lastDirection = "up";
-      scrollCounter++;
-      if ((scrollCounter >= scrollStep) && (freeTimeDiv.getBoundingClientRect().top > windowHeight + 10)) {
-        const addedLetters = freeTimeText.slice(freeTimeTitle.textContent.length, freeTimeTitle.textContent.length + 1);
-        sectionTitle.textContent = sectionTitle.textContent.slice(0, path.length) + sectionTitle.textContent.slice(path.length + 1);
-        freeTimeTitle.textContent += addedLetters;
-        if (sectionTitle.textContent.length == path.length) sectionTitle.textContent = "";
-        scrollCounter = 0;
-      }
-    }
-  }
+//     if (currentScroll < lastScroll) {
+//       if (lastDirection == "down") scrollCounter = 0;
+//       lastDirection = "up";
+//       scrollCounter++;
+//       if ((scrollCounter >= scrollStep) && (freeTimeDiv.getBoundingClientRect().top > windowHeight + 10)) {
+//         const addedLetters = freeTimeText.slice(freeTimeTitle.textContent.length, freeTimeTitle.textContent.length + 1);
+//         sectionTitle.textContent = sectionTitle.textContent.slice(0, path.length) + sectionTitle.textContent.slice(path.length + 1);
+//         freeTimeTitle.textContent += addedLetters;
+//         if (sectionTitle.textContent.length == path.length) sectionTitle.textContent = "";
+//         scrollCounter = 0;
+//       }
+//     }
+//   }
 
-  if (freeTimeTitle.textContent != freeTimeText) {
-    edu.style.position = "fixed"; 
-    edu.style.top = -1000 + "px";
-    TAAG.style.top = -1000 + "px";
-    effectStarted = true;
-  } else { 
-    edu.style.position = "relative";
-    edu.style.top = 'auto'; 
-    TAAG.style.top = saveTAAGPosition.top + "px";
-  }
+//   if (freeTimeTitle.textContent != freeTimeText) {
+//     edu.style.position = "fixed"; 
+//     edu.style.top = -1000 + "px";
+//     TAAG.style.top = -1000 + "px";
+//     effectStarted = true;
+//   } else { 
+//     edu.style.position = "relative";
+//     edu.style.top = 'auto'; 
+//     TAAG.style.top = saveTAAGPosition.top + "px";
+//   }
 
-  //!THIS WAS TO FIX A BUG THAT MIGHT HAPPEN IF THE USER SWITCHES BETWEEN MOUSE AND TRACKPAD WHILE SCROLLING, HOWEVER THE BUG IS VERY MINOR AND THE END RESULT IS MUCH WORSE IF THE SCRIPT HANDLES IT
-  // if(effectStarted && edu.style.position == "relative" && window.scrollY < window.innerHeight)
-  //   {
-  //     if(freeTimeTitle.classList.contains("free-time-fixed")) freeTimeTitle.classList = "free-time-static";
-  //   }
-}); 
+//   //!THIS WAS TO FIX A BUG THAT MIGHT HAPPEN IF THE USER SWITCHES BETWEEN MOUSE AND TRACKPAD WHILE SCROLLING, HOWEVER THE BUG IS VERY MINOR AND THE END RESULT IS MUCH WORSE IF THE SCRIPT HANDLES IT
+//   // if(effectStarted && edu.style.position == "relative" && window.scrollY < window.innerHeight)
+//   //   {
+//   //     if(freeTimeTitle.classList.contains("free-time-fixed")) freeTimeTitle.classList = "free-time-static";
+//   //   }
+// }); 
 
 window.addEventListener('scroll', () => {
   lastScrollY = window.scrollY;
@@ -138,50 +143,79 @@ function handleScroll(scrollY) {
     TAAG.style.filter = `blur(${blurAmount}px)`;
   }
 
-  if (freeTimeTitle.textContent != "") {
-    freeTimeDiv.style.position = "absolute";
-    freeTimeDiv.style.opacity= "0"; 
-    freeTimeDiv.style.top = `${scrollY + 1000}px`;
-  }
-  else
-  {
-    freeTimeDiv.style.opacity= "1";
-  }
+  // if (freeTimeTitle.textContent != "") {
+  //   freeTimeDiv.style.position = "absolute";
+  //   freeTimeDiv.style.opacity= "0"; 
+  //   freeTimeDiv.style.top = `${scrollY + 1000}px`;
+  // }
+  // else
+  // {
+  //   freeTimeDiv.style.opacity= "1";
+  // }
 
-  const freeTimeMidPoint = freeTimeTitle.getBoundingClientRect().top + (freeTimeTitle.getBoundingClientRect().height / 2);
+  // const freeTimeMidPoint = freeTimeTitle.getBoundingClientRect().top + (freeTimeTitle.getBoundingClientRect().height / 2);
 
-  if (freeTimeTitle.classList.contains("free-time-static") && locked && freeTimeMidPoint > (windowHeight / 2 + 1)) locked = false;
+  // if (freeTimeTitle.classList.contains("free-time-static") && locked && freeTimeMidPoint > (windowHeight / 2 + 1)) locked = false;
 
-  if (freeTimeTitle.classList.contains("free-time-fixed") && (freeTimeTitle.getBoundingClientRect().top - edu.getBoundingClientRect().bottom <= 2.5)) {
-    freeTimeEffect = false; 
-    freeTimeTitle.classList = "free-time-static";
-  }
+  // if (freeTimeTitle.classList.contains("free-time-fixed") && (freeTimeTitle.getBoundingClientRect().top - edu.getBoundingClientRect().bottom <= 2.5)) {
+  //   freeTimeEffect = false; 
+  //   freeTimeTitle.classList = "free-time-static";
+  // }
 
-  if (freeTimeMidPoint <= windowHeight / 2 && !locked) {
-    freeTimeTitle.classList = "free-time-fixed";
-    freeTimeTitle.style.top = `${windowHeight / 2 - (freeTimeTitle.getBoundingClientRect().height / 2)}px`;
-    freeTimeTitle.style.left = `${secondSection.getBoundingClientRect().left + 25}px`;
-    locked = true;
-  }
+  // if (freeTimeMidPoint <= windowHeight / 2 && !locked) {
+  //   freeTimeTitle.classList = "free-time-fixed";
+  //   freeTimeTitle.style.top = `${windowHeight / 2 - (freeTimeTitle.getBoundingClientRect().height / 2)}px`;
+  //   freeTimeTitle.style.left = `${secondSection.getBoundingClientRect().left + 25}px`;
+  //   locked = true;
+  // }
 
-  if (edu.getBoundingClientRect().bottom < -15) {
-    if(!freeTimeEffect) freeTimeEffect = true;
-  }
-  lastScroll = scrollY;
+  // if (edu.getBoundingClientRect().bottom < -15) {
+  //   if(!freeTimeEffect) freeTimeEffect = true;
+  // }
+  // lastScroll = scrollY;
 }
 
 window.onresize = (e) => { //!TODO: education tab postion has to be changed on window resizing
   console.log("Window Resized");
   screenCenter = window.innerWidth / 2;
   windowHeight = window.innerHeight;
+  let additionalMargin = window.innerHeight * heightFactor;
+  edu.style.marginTop = `${TAAG.getBoundingClientRect().height + baseMargin - additionalMargin}px`;
   centerElement(elementsToCenter);
+  console.log(screenCenter);
   if (freeTimeTitle.classList.contains("free-time-fixed")) freeTimeTitle.style.top = `${windowHeight / 2 - (freeTimeTitle.getBoundingClientRect().height / 2)}px`; //? Change text position so that it's always centered based on window height
 }
 
 function centerElement(elements) {
-  screenCenter = window.innerWidth / 2;
+  const screenCenter = window.innerWidth / 2;
+  //console.log(edu.style.width);
+  //setResponsiveMargin();
   elements.forEach(elem => {
-    elem.style.marginLeft = `${screenCenter / 4}px`;
-    elem.style.marginRight = `${screenCenter / 4}px`;
+    console.log(elem);
+
+    const elemWidth = elem.offsetWidth;
+
+    if (getComputedStyle(elem).position === "absolute" || getComputedStyle(elem).position === "fixed") 
+    {
+      elem.style.left = "50%";
+      elem.style.transform = "translateX(-50%)";
+    } else {
+      elem.style.marginLeft += `${screenCenter - elemWidth/2}px`;
+      elem.style.marginRight += `${screenCenter - elemWidth/2}px`;
+    }
   });
 }
+
+// function setResponsiveMargin() {
+//   if (!edu) return;
+
+//   const referenceHeight = 1026; // This is the height of the 17" display where 25% worked best.
+//   const referenceMargin = referenceHeight * 0.25; // 25% of 1026
+//   let currentHeight = window.innerHeight;
+
+//   let calculatedMargin = (referenceMargin / referenceHeight) * currentHeight;
+
+//   console.log(`Setting margin-top: ${calculatedMargin}px for edu`);
+  
+//   edu.style.marginTop = `${calculatedMargin}px`;
+//}
